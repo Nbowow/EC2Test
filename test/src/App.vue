f22daae7884d5fcd513089c66176ff71f60458cd
Nbowow <nbo5548@naver.com>
2024-12-07 10:28:38 +0900

    first commit

---
+++ test/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <h1>Vue.js with Spring Boot API</h1>
+    <div>
+      <button @click="fetchMessage">Get Message</button>
+      <p>{{ message }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "",
+    };
+  },
+  methods: {
+    async fetchMessage() {
+      try {
+        const response = await fetch(
+          "http://<EC2-Instance-IP>:8080/api/message"
+        );
+        this.message = await response.json().message;
+      } catch (error) {
+        console.error("Error fetching message:", error);
+      }
+    },
+  },
+};
+</script>
